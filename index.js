b34dec0685b724f444fdb14fd542bbcb11a3bb69
Adri <62384358+Adri20079@users.noreply.github.com>
2020-09-24 22:13:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const { PREFIX, AUTHOR} = require('./config');
+
+const client = new Discord.Client({ 
+    disableEveryone: true 
+});
+
+client.on('ready', async () => {
+    console.log('Kaguya => Discord.Connected');
+    client.user.setActivity(`${PREFIX}verify | Developed By ${AUTHOR}`, { type: 'PLAYING' });
+});
+
+
+client.on('message', message => {
+    if(message.author.bot) return;
+        
+    const content = message.content.split(' ');
+    const command = content[0].toLowerCase();
+    const args = content.slice(1);
+
+    const member = message.member
+
+    if (command === `${PREFIX}verify`) {
+        if (member.guild.id == '736687689151283311') {
+            try{
+                const role = member.guild.roles.cache.find(role => role.name === 'Member');
+                member.roles.add(role);
+                member.send("✅Verified!")
+            } catch(e) {
+                console.error(e);
+                member.send("❌Failed To Verify!")
+            }
+        }
+
+        if (member.guild.id == '756131619634544750') {
+            try{
+                const role = member.guild.roles.cache.find(role => role.name === 'Member');
+                member.roles.add(role);
+                member.send("✅Verified!")
+            } catch(e) {
+                console.error(e);
+                member.send("❌Failed To Verify!")
+            }
+        }
+    }
+});
+
+client.login(process.env.TOKEN);
